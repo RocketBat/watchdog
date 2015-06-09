6ef48a94220d9aa16fb0c19cf9a15ab1c47d8dc5
Mikhail Kozlov <Coffe-man@mail.ru>
2015-06-09 08:05:13 +0000

    watchdog 9

--- watch_dog.pl
+++ watch_dog.pl
@@ -4,6 +4,12 @@ use strict;
 use warnings;
 use File::stat;
 
+my $a=0; #----dropi
+my $b=0; #----chtenie loga
+my $c=0; #----vkl li voobshe dpi
+my $d=0; #----vkl li voobshe bypass
+my $chk=0; #-------schetchik
+
 my $max_drops = 0.04;
 #print "Устанавливаем макс. значение потерь = $max_drops*100 %\n"; 
 
@@ -33,13 +39,14 @@ while (my $file = readdir(DIR)) {
 #		print "Droprate in this moment $1\n";
 
         	if($drop_rate1 > $max_drops){
-        
+        		$a=1;
 			print "$datestring Drops level $drop_rate1 exceeds the configured maximum of $max_drops\n";
 #-----------------------------Vkl bypass
 		        system('echo "with LOL by the pass"');
 #------------------------------	
-			system("echo $datestring 'bypass is on' >> ./bypass.log");
+			system("echo $datestring 'bypass is on, droprate is = $drop_rate1' >> ./bypass.log");
 			print "Bypass is switched on.\n";
+			$d=1;
 #--------Proverka 4to dropi = 0 i perrevod traffica na osnovu
 			
       			for (my $i=0;$i < 3;$i++){ 
@@ -54,7 +61,8 @@ while (my $file = readdir(DIR)) {
 				if ($drop_rate1 == 0) {
 					print "Check.And if al $co perevod na osnovu.\n";
 					print "Dropi $drop_rate1\n";
-					if ($i == 2) {print "Transfer traffic to main\n";}
+					if ($i == 2) {print "Transfer traffic to main\n"; 
+							$d=0;}
 				}
 				else {
 				      print "Anything wrong stay bypass.\n";
@@ -67,26 +75,20 @@ while (my $file = readdir(DIR)) {
 		}
 	
 		else{
+			$a=0;
 	          	print "$datestring Drops level $drop_rate1 is in normal range\n";
                         }      
  		
       }
       else{
+		$b=1;
         	print "$datestring Can not read drop rate\n";
 #------------Vkl bypass
 		system('echo "with LOL by the pass"');
 #---------------------
-		system("echo $datestring 'bypass is on' >> ./bypass.log");
+		system("echo $datestring 'bypass is on, Can not read drop rate' >> ./bypass.log");
 		print "Bypass is switched on.\n";
-		
-	
-		my $mtime = (stat $log_file)[9];
-			if ($mtime =~s/^(\d+)/localtime($1)/e) {
-				print "$datestring LOg file updating \n";
-				system('echo "snimaem s bypassa na osnovu"');
-			}
-			else {sleep 10;}
-#        	exit;
+		$d=1;
       }
 
 #---------------check process
@@ -94,16 +96,19 @@ while (my $file = readdir(DIR)) {
 
         my $process_status = `ps afx | grep "dpi-engine" | grep -v grep`;
 	if ($process_status eq ""){
+		$c=1;
         	print "$datestring Did not find DPI process in process list\n";
 #---------Vkl Bypass
         	system('echo "with LOL by the pass"');
 #------------------	
-		system("echo $datestring 'bypass on' >> ./bypass.log");
+		system("echo $datestring 'bypass on, Did not find DPI process in process list' >> ./bypass.log");
 	        print "Bypass is switched on.\n";
+		$d=1;
 #tyt kagdie 10 sec proveryaem vkl li bypass
         	
       }
       else{
+		$c=0;
 #	       	print "$datestring Found DPI process in process list\n";
 		system("echo $datestring 'Found DPI process in process list' >> bypass.log");
       }
@@ -111,19 +116,32 @@ while (my $file = readdir(DIR)) {
 
 
 #----proverka obnovlyaetsya li fail
- 	my $mtime = (stat $log_file)[9];
-	
-                   if ($mtime =~s/^(\d+)/localtime($1)/e) {
-                        print "$mtime   -   localtime\n";     
+ 	my $mt = stat($log_file);
+	my $st = $mt -> mtime;
+#	print "$st\n";
+                 if ($st =~s/^(\d+)/localtime/e) {
+                       $b=0;
 			print "$datestring Log file updating \n";
                    }
                    else {
-	
-			#---------Vkl Bypass
+			$b=1;
+			print "$datestring Achtung! Log does not updating!\n";
+#---------------------Vkl Bypass
 	                system('echo "with LOL by the pass"');
-			#------------------ 
-			system("echo $datestring 'bypass on' >> ./bypass.log");			
+			$d=1;
+#------------------------
+			system("echo $datestring 'bypass on, Log does not updating!' >> ./bypass.log");			
 			sleep 5;
+			}
+
+	if ($a==0 && $b==0 && $c==0 && $d==0) {
+		print "vse good";
+#		system("echo $datestring 'bypass off' >> ./bypass.log");
+		}
+	elsif ($a==0 && $b==0 && $c==0) {print "ostaemsa na bypasse  \n";
+			$chk++;
+			if ($chk==3) {print "bypass off";}
+	else {sleep 5;}
 			}
 
       sleep 5;
