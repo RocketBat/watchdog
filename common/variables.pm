b8859ae89d1a81fa5d845ec9619d3e6ec96eb187
Mikhail Kozlov <Coffe-man@mail.ru>
2015-08-04 17:42:02 +0600

    change log directory

--- common/variables.pm
+++ common/variables.pm
@@ -35,8 +35,13 @@ our $refresh_timer = $config->{main}->{refresh_timer}; #----speed of logging
 our $delay_removal_from_bypass = $config->{main}->{delay_removal_from_bypass}; # this delay needs when Bypass is turned off earlier than necessary
 our $readDropRateDelay = $config->{main}->{readDropRateDelay}; # how many times needs to be in "can not read drop rate" state
 our $shaper_type = $config->{dpi}->{shaper};
-our $watchdog_log = '/usr/adm/watchdog/logs/bypass.log'; #---CHECK FULL PATH
 our $directory = '/usr/adm/adm_s1/logs/';
+if ($revision eq "revision") {
+    our $watchdog_log = '/usr/adm/watchdog/logs/bypass.log'; #---CHECK FULL PATH
+}
+elsif ($revison eq "debug") {
+    our $watchdog_log = '/home/mihail/Develop/Watch_dog/bypass.log';
+}
 
 #--secondary variables
 our $datestring = strftime "%F %T", localtime;
