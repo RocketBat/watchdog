66c607bebbc4a83c1b9705fa9ebb0e01763d3b9d
Mikhail Kozlov <Coffe-man@mail.ru>
2015-08-05 17:07:58 +0600

    fix

--- modules/logging.pm
+++ modules/logging.pm
@@ -33,7 +33,7 @@ sub outlog {
 	system("echo $datestring $logmsg_zcheck >>  $watchdog_log");
 	system("echo $datestring $logmsg_fresh >>  $watchdog_log");
 	system("echo $datestring $logmsg_cdrops ' drop ' $drop_rate1 ' and ' $drop_rate2 >>  $watchdog_log");	
-	fork(sub => sub { sleep 2 });
+	Forks::Super::pause(5);
 }
 
 1;
