8f2dc7f400aadd4457357a38d062cefb9255f111
Mikhail Kozlov <Coffe-man@mail.ru>
2015-08-07 12:08:55 +0600

    fix restart module/ add librarry

--- scripts/restart.pm
+++ scripts/restart.pm
@@ -19,6 +19,8 @@ BEGIN {
 #--my libraries
 use lib '/home/mihail/Develop/Watch_dog/common';
 use common::variables;
+use lib '/home/mihail/Develop/Watch_dog/modules';
+use modules::logging;
 
 sub restart {
 	$CWD = '/usr/adm/adm_s1';
