b2ec691453c150c7c119d485dfc05c049661ad62
Mikhail Kozlov <Coffe-man@mail.ru>
2015-08-19 16:27:03 +0600

    set mspeed variables as global

--- common/variables.pm
+++ common/variables.pm
@@ -14,7 +14,7 @@ BEGIN {
     require Exporter;
     our @ISA         = qw(Exporter);
     # Functions and variables which are exported by default
-    our @EXPORT      = qw($logmsg $logtime_delay $logmsg_zcheck $logmsg_cdrops $logmsg_fresh $logmsg_proc $log_params $shaper_type $revision $readDropRateDelay $server $t1 $t2 $log_file $textmsg_zcheck $textmsg_cdrops $textmsg_fresh $textmsg_proc $max_drops $datestring $directory $date $stat $refresh_timer $text_out $drop_rate1 $drop_rate2 $bypass_on_time $bypass_off_time $watchdog_log $delay_removal_from_bypass $droprate_read);
+    our @EXPORT      = qw($mspeed2 $mspeed1 $logmsg $logtime_delay $logmsg_zcheck $logmsg_cdrops $logmsg_fresh $logmsg_proc $log_params $shaper_type $revision $readDropRateDelay $server $t1 $t2 $log_file $textmsg_zcheck $textmsg_cdrops $textmsg_fresh $textmsg_proc $max_drops $datestring $directory $date $stat $refresh_timer $text_out $drop_rate1 $drop_rate2 $bypass_on_time $bypass_off_time $watchdog_log $delay_removal_from_bypass $droprate_read);
 }
 
 #--open the config
@@ -59,6 +59,8 @@ our $droprate_read = 0; # need for check_drop function? when can not read drop r
 our $log_file;
 our $t1 = 0;
 our $t2 = 0;
+our $mspeed1 = 0;
+our $mspeed2 = 0;
 
 #variables for output information
 our $textmsg_proc; #---text message processcheck
